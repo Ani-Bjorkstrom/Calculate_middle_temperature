5f961321b5b456ad6f1f6b37d4a26e96be467346
Ani Davtyan <missanidavtyan@gmail.com>
2021-04-19 07:39:56 +0200

    Added comments

--- js/test.js
+++ js/test.js
@@ -1,10 +1,24 @@
+/* 
+function tableRow() {
+    var table = document.getElementById("temperature-table");
+    var rows = table.getElementsByTagName("tr");
+    return rows;
+}
+*/
+
 function calculateAverage(){
+    //saves the reference to table node
     var table = document.getElementById('temperature-table');
+    //saves reference to rows node list in the table
     var rows = table.getElementsByTagName('tr');
+        //rotates over the rows and saves the rows' node list per line in a variable called cells
         for (var i = 1; i < rows.length; i++){
             var cells = rows[i].getElementsByTagName('td');
+            //calls averageValue function with parameter cells 
             var averageTemp = averageValue(cells);
+            //chooses the last cell in a row node list
             var averageCell = cells[cells.length-1];
+            //gives the last data cell of each row average value of the cells per row
             averageCell.textContent = averageTemp;
         }
     }
@@ -21,20 +35,54 @@ function averageValue(cells){
 }
 
 function findTemperatures(){
+    clearFormatting();
+    //saves the input button's node element in minTemp variable
     var minTemp = parseInt(document.getElementsById('min-temp').value);
-    if(isNaN(minTemp)){
-        alert("Enter a number")
-        findTemperatures();
-    }
     var table = document.getElementById("temperature-table"); 
     var rows = table.getElementsByTagName("tr"); 
+
+
     for (var i = 1; i < rows.length; i++){
         var cells = rows[i].getElementsByTagName("td");
+        if (matchesTemp(minTemp, cells)){
+            rows[i].className = "markedRow";
+        }
     }
 }
 
 function matchesTemp(minTemp, cells){
-
+    var hasValue = false;
+    for (var i = 1; i < cells.length - 1; i++){
+        //saves the current value of the datacell
+        var currentValue = parseInt(cells[i].textContent)
+        //for each current value checks if it's bigger or equal to input value in minTemp field
+        if (currentValue >= minTemp) {
+            //sets the hasValue to true
+            hasValue = true;
+            cells[i].className = "markedCell"
+        }
+    }
+    return hasValue;
 }
-calculateAverage()
+
+function clearFormatting (){
+    var table = document.getElementById("temperature-table");
+    var rows = table.getElementsByTagName("tr");
+
+   for(var i = 1; i < rows.length; i++){
+       rows[i].className = "";
+       var cells = rows[i].getElementsByTagName("td");
+       for (var j = 1; j < cells.length - 1; j++){
+           cells[j].className = "";
+       }
+   }
+}
+
+calculateAverage();
+var searchButton = document.getElementById("show-value");
+searchButton.addEventListener("click", findTemperatures, false);
+
+var clearButton = document.getElementsById("clear-formatting");
+clearButton.addEventListener("click", clearFormatting, false);
+
 
